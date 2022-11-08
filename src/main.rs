f402a5489de3404ef9ba9ea269f326666be20227
asimdahall <dahalasim1997@gmail.com>
2022-11-08 22:54:26 +0100

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x: u32 = 100;
+    println!("The value of X is {}", x);
+
+    // @info: this is called shadowing, you can user the same variable with the let keyword.
+    let x: i32 = -20;
+    println!("The value of X is {}", x);
+
+    let y: bool = false;
+    println!("The value of Y is {}", y);
 }
